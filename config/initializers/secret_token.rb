2409c408961f15e93fec4117cc8fbf9fedddd813
Travis Johnson <travis@integritydesigns.com>
2012-10-05 15:32:12 -0400

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wittty::Application.config.secret_token = '818d94fec955ac248370f26430b61676cf2879545a583676e3dcb9efc138c52f5293de8c3664f63751ca17e32a16c82c695e780740a31bf9f005673a17bf729d'
